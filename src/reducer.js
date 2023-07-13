86928aea826a44c2d1774f8780662b9cc88a6dd0
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-07-13 01:22:41 +0100

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+import {
+  CLEAR_CART,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions"
+
+const reducer = (state, action) => {
+  return state
+}
+
+export default reducer
