87e0495fd8bc8069e391e99a01c3922f5abaa760
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-07-14 10:47:28 +0100

    add clearCart, increase, decrease, remove functionalities

--- src/context.jsx
+++ src/context.jsx
@@ -2,25 +2,46 @@ import { useContext, useReducer, useEffect, createContext } from "react"
 import reducer from "./reducer"
 import {
   CLEAR_CART,
+  REMOVE,
   INCREASE,
   DECREASE,
   LOADING,
   DISPLAY_ITEMS,
 } from "./actions"
+import cartItems from "./data"
 
 const AppContext = createContext()
 
 const initialState = {
   loading: false,
-  cart: [],
-  v,
+  cart: new Map(cartItems.map((item) => [item.id, item])),
 }
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
+  const remove = (id) => {
+    dispatch({ type: REMOVE, id })
+  }
+
+  const increase = (id) => {
+    dispatch({ type: INCREASE, id })
+  }
+
+  const decrease = (id) => {
+    dispatch({ type: DECREASE, id })
+  }
+
   return (
-    <AppContext.Provider value={{ ...state }}> {children} </AppContext.Provider>
+    <AppContext.Provider
+      value={{ ...state, clearCart, remove, increase, decrease }}
+    >
+      {children}
+    </AppContext.Provider>
   )
 }
 
